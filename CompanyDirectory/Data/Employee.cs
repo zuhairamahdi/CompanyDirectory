8c57d1f5a04ddee0fc36b3aa6fd705359dd9643b
Zuhair Abdulla <zuhairamahdi@gmail.com>
2019-11-16 15:15:24 +0300

    Add Employee model

---
+++ CompanyDirectory/Data/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyDirectory.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FistName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public Department Department { get; set; }
+        public Employee ReportsTo { get; set; }
+    }
+}
