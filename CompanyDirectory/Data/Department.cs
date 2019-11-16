16ddcb1cb4a4651ab96877c0ac983055ff809a66
Zuhair Abdulla <zuhairamahdi@gmail.com>
2019-11-16 14:22:23 +0300

    Add Database project

---
+++ CompanyDirectory/Data/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyDirectory.Data
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+    }
+}
