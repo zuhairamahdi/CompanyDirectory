f5718ae9e661147097d9f548b997ba4e650b20af
Zuhair Abdulla <zuhairamahdi@gmail.com>
2019-11-16 19:12:07 +0300

    Add Identity framework scaffolding files for customization

---
+++ CompanyDirectory/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CompanyDirectory.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CompanyDirectory.Areas.Identity.IdentityHostingStartup))]
+namespace CompanyDirectory.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
